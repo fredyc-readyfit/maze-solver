13b2a271632ad0521e42e4f82d0e8a1d2f7c0a96
Fredy Augusto Castellon Bedoya <fredyc@readyfit.com>
2024-12-15 23:11:18 -0500

    feat: cell class and draw_move method

---
+++ cell.py
@@ -0,0 +1,49 @@
+from line import Line
+from point import Point
+from window import Window
+
+class Cell:
+    def __init__(self, x1, x2, y1, y2, win: Window, has_left_wall = True, has_right_wall = True, has_top_wall = True, has_bottom_wall = True):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+
+    def draw(self):
+        top_left = Point(self._x1, self._y1)
+        top_right = Point(self._x2, self._y1)
+        bottom_left = Point(self._x1, self._y2)
+        bottom_righ = Point(self._x2, self._y2)
+        
+        if self.has_left_wall:
+            left_wall = Line(top_left, bottom_left)
+            left_wall.draw(self._win.canvas, 'black')
+
+        if self.has_top_wall:
+            top_wall = Line(top_left, top_right)
+            top_wall.draw(self._win.canvas, 'black')
+
+        if self.has_right_wall:
+            righ_wall = Line(top_right, bottom_righ)
+            righ_wall.draw(self._win.canvas, 'black')
+        
+        if self.has_bottom_wall:
+            bottom_wall = Line(bottom_left, bottom_righ)
+            bottom_wall.draw(self._win.canvas, 'black')
+    
+    def draw_move(self, to_cell, undo = False):
+        center = Point(self._x1 + abs((self._x2 - self._x1) / 2), self._y1 + abs((self._y2 - self._y1)/2))
+        to_center = Point(to_cell._x1 + abs((to_cell._x2 - to_cell._x1) / 2), to_cell._y1 + abs((to_cell._y2 - to_cell._y1) / 2))
+        move = Line(center, to_center)
+
+        if not undo:
+            color = 'red'
+        else:
+            color = 'gray'
+
+        move.draw(self._win.canvas, color) 
--- main.py
+++ main.py
@@ -1,12 +1,51 @@
 from window import Window
-from line import Line
-from point import Point
+# from line import Line
+# from point import Point
+from cell import Cell
 
 def main():
     win = Window(800, 800)
-    win.draw_line(Line(Point(100, 100), Point(700, 700)), "red")
-    win.draw_line(Line(Point(700, 100), Point(100, 700)), "green")
-    win.draw_line(Line(Point(100, 100), Point(700, 100)), "blue")
+    # win.draw_line(Line(Point(100, 100), Point(700, 700)), "red")
+    # win.draw_line(Line(Point(700, 100), Point(100, 700)), "green")
+    # win.draw_line(Line(Point(100, 100), Point(700, 100)), "blue")
+
+    # Cell(100, 200, 100, 200, win).draw()
+    # Cell(150, 175, 50, 75, win, has_left_wall=False).draw()
+    # Cell(350, 400, 350, 400, win, has_top_wall=False).draw()
+    # Cell(100, 400, 150, 200, win, has_right_wall=False).draw()
+    # Cell(600, 700, 600, 700, win, has_bottom_wall=False).draw()
+
+    c1 = Cell(10, 30, 10, 30, win,  has_right_wall=False)
+    c2 = Cell(30, 50, 10, 30, win, has_left_wall=False, has_right_wall=False)
+    c3 = Cell(50, 70, 10, 30, win, has_left_wall=False, has_bottom_wall=False)
+    c4 = Cell(50, 70, 30, 50, win, has_top_wall=False, has_bottom_wall=False)
+    c5 = Cell(50, 70, 50, 70, win, has_top_wall=False, has_left_wall=False)
+    c6 = Cell(30, 50, 50, 70, win, has_top_wall=False, has_left_wall=False, has_right_wall=False)
+    c7 = Cell(30, 50, 30, 50, win, has_bottom_wall=False)
+    c8 = Cell(10, 30, 50, 70, win, has_right_wall=False, has_bottom_wall=False)
+    c9 = Cell(10, 30, 70, 90, win, has_top_wall=False, has_bottom_wall=False)
+
+    c1.draw()
+    c2.draw()
+    c3.draw()
+    c4.draw()
+    c5.draw()
+    c6.draw()
+    c7.draw()
+    c8.draw()
+    c9.draw()
+    
+    c1.draw_move(c2)
+    c2.draw_move(c3)
+    c3.draw_move(c4)
+    c4.draw_move(c5)
+    c5.draw_move(c6)
+    c6.draw_move(c7)
+    c7.draw_move(c6, undo=True)
+    c6.draw_move(c8)
+    c8.draw_move(c9)
+    
+
     win.wait_for_close()
 
 if __name__ == '__main__':
